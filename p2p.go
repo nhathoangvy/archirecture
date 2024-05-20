4dee9394e3a69093708fa761956a4d660840099f
Hoangvn <nhathoangvy@gmail.com>
2024-05-20 12:29:23 +0700

    Update p2p.go

--- p2p.go
+++ p2p.go
@@ -1,135 +1,206 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// Package main implements a simple example demonstrating a Pion-to-Pion ICE connection
 package main
 
 import (
+	"bufio"
 	"context"
-	"encoding/binary"
 	"flag"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p/p2p/discovery"
-	"github.com/multiformats/go-multiaddr"
+	"github.com/pion/ice/v3"
+	"github.com/pion/logging"
+	"github.com/pion/randutil"
 )
 
-const protocolID = "/example/1.0.0"
-const discoveryNamespace = "example"
+//nolint:gochecknoglobals
+var (
+	isControlling                                              bool
+	iceAgent                                                   *ice.Agent
+	remoteAuthChannel                                          chan string
+	localHTTPPort, remoteHTTPPort, localMuxPort, remoteMuxPort int
+)
 
-func main() {
-	// Add -peer-address flag
-	peerAddr := flag.String("peer-address", "", "peer address")
+// HTTP Listener to get ICE Credentials from remote Peer
+func remoteAuth(_ http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	remoteAuthChannel <- r.PostForm["ufrag"][0]
+	remoteAuthChannel <- r.PostForm["pwd"][0]
+}
+
+// HTTP Listener to get ICE Candidate from remote Peer
+func remoteCandidate(_ http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	c, err := ice.UnmarshalCandidate(r.PostForm["candidate"][0])
+	if err != nil {
+		panic(err)
+	}
+
+	if err := iceAgent.AddRemoteCandidate(c); err != nil { //nolint:contextcheck
+		panic(err)
+	}
+}
+
+func main() { //nolint
+	var (
+		err  error
+		conn *ice.Conn
+	)
+	remoteAuthChannel = make(chan string, 3)
+
+	flag.BoolVar(&isControlling, "controlling", false, "is ICE Agent controlling")
+	flag.BoolVar(&isControlling, "middle", false, "is ICE Agent middle")
 	flag.Parse()
 
-	// Create the libp2p host.
-	//
-	// Note that we are explicitly passing the listen address and restricting it to IPv4 over the
-	// loopback interface (127.0.0.1).
-	//
-	// Setting the TCP port as 0 makes libp2p choose an available port for us.
-	// You could, of course, specify one if you like.
-	host, err := libp2p.New(context.Background(), libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"))
-	if err != nil {
+	if isControlling {
+		localMuxPort = 3000
+		localHTTPPort = 9000
+		remoteHTTPPort = 9001
+	} else {
+		localMuxPort = 3001
+		localHTTPPort = 9001
+		remoteHTTPPort = 9000
+	}
+
+	http.HandleFunc("/remoteAuth", remoteAuth)
+	http.HandleFunc("/remoteCandidate", remoteCandidate)
+	go func() {
+		if err = http.ListenAndServe(fmt.Sprintf(":%d", localHTTPPort), nil); err != nil { //nolint:gosec
+			panic(err)
+		}
+	}()
+
+	if isControlling {
+		fmt.Println("Local Agent is controlling")
+	} else {
+		fmt.Println("Local Agent is controlled")
+	}
+	fmt.Print("Press 'Enter' when both processes have started")
+	if _, err = bufio.NewReader(os.Stdin).ReadBytes('\n'); err != nil {
 		panic(err)
 	}
-	defer host.Close()
 
-	// Print this node's addresses and ID
-	fmt.Println("Addresses:", host.Addrs())
-	fmt.Println("ID:", host.ID())
+	addr := &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: localMuxPort}
+	mUdpServer, err := net.ListenUDP("udp4", addr)
+	if err != nil {
+		log.Print(err)
+	}
 
-	// Setup a stream handler.
-	//
-	// This gets called every time a peer connects and opens a stream to this node.
-	host.SetStreamHandler(protocolID, func(s network.Stream) {
-		go writeCounter(s)
-		go readCounter(s)
+	loggerFactory := logging.NewDefaultLoggerFactory()
+	udpMux := ice.NewUDPMuxDefault(ice.UDPMuxParams{
+		Logger:  loggerFactory.NewLogger("ice"),
+		UDPConn: mUdpServer,
 	})
 
-	// Setup peer discovery.
-	discoveryService, err := discovery.NewMdnsService(
-		context.Background(),
-		host,
-		time.Second,
-		discoveryNamespace,
-	)
+	iceAgent, err = ice.NewAgent(&ice.AgentConfig{
+		NetworkTypes:    []ice.NetworkType{ice.NetworkTypeUDP4},
+		CandidateTypes:  []ice.CandidateType{ice.CandidateTypeHost},
+		UDPMux:          udpMux,
+		IncludeLoopback: addr.IP.IsLoopback(),
+	})
 	if err != nil {
 		panic(err)
 	}
-	defer discoveryService.Close()
 
-	discoveryService.RegisterNotifee(&discoveryNotifee{h: host})
+	// a, err := ice.NewAgent(&ice.AgentConfig{
+	// 	CandidateTypes: []ice.CandidateType{ice.CandidateTypeHost},
+	// })
 
-	// If we received a peer address, we should connect to it.
-	if *peerAddr != "" {
-		// Parse the multiaddr string.
-		peerMA, err := multiaddr.NewMultiaddr(*peerAddr)
+	// When we have gathered a new ICE Candidate send it to the remote peer
+	if err = iceAgent.OnCandidate(func(c ice.Candidate) {
+		if c == nil {
+			return
+		}
+
+		_, err = http.PostForm(fmt.Sprintf("http://localhost:%d/remoteCandidate", remoteHTTPPort), //nolint
+			url.Values{
+				"candidate": {c.Marshal()},
+			})
 		if err != nil {
 			panic(err)
 		}
-		peerAddrInfo, err := peer.AddrInfoFromP2pAddr(peerMA)
-		if err != nil {
-			panic(err)
-		}
-
-		// Connect to the node at the given address.
-		if err := host.Connect(context.Background(), *peerAddrInfo); err != nil {
-			panic(err)
-		}
-		fmt.Println("Connected to", peerAddrInfo.String())
-
-		// Open a stream with the given peer.
-		s, err := host.NewStream(context.Background(), peerAddrInfo.ID, protocolID)
-		if err != nil {
-			panic(err)
-		}
-
-		// Start the write and read threads.
-		go writeCounter(s)
-		go readCounter(s)
+	}); err != nil {
+		panic(err)
 	}
 
-	sigCh := make(chan os.Signal)
-	signal.Notify(sigCh, syscall.SIGKILL, syscall.SIGINT)
-	<-sigCh
-}
+	// When ICE Connection state has change print to stdout
+	if err = iceAgent.OnConnectionStateChange(func(c ice.ConnectionState) {
+		fmt.Printf("ICE Connection State has changed: %s\n", c.String())
+	}); err != nil {
+		panic(err)
+	}
 
-func writeCounter(s network.Stream) {
-	var counter uint64
+	// Get the local auth details and send to remote peer
+	localUfrag, localPwd, err := iceAgent.GetLocalUserCredentials()
+	if err != nil {
+		panic(err)
+	}
 
+	_, err = http.PostForm(fmt.Sprintf("http://localhost:%d/remoteAuth", remoteHTTPPort), //nolint
+		url.Values{
+			"ufrag": {localUfrag},
+			"pwd":   {localPwd},
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	remoteUfrag := <-remoteAuthChannel
+	remotePwd := <-remoteAuthChannel
+
+	if err = iceAgent.GatherCandidates(); err != nil {
+		panic(err)
+	}
+
+	// Start the ICE Agent. One side must be controlled, and the other must be controlling
+	if isControlling {
+		conn, err = iceAgent.Dial(context.TODO(), remoteUfrag, remotePwd)
+	} else {
+		conn, err = iceAgent.Accept(context.TODO(), remoteUfrag, remotePwd)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// Send messages in a loop to the remote peer
+	go func() {
+		for {
+			time.Sleep(time.Second * 3)
+
+			val, err := randutil.GenerateCryptoRandomString(15, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+			if err != nil {
+				panic(err)
+			}
+			if _, err = conn.Write([]byte(val)); err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("Sent: '%s'\n", val)
+		}
+	}()
+
+	// Receive messages in a loop from the remote peer
+	buf := make([]byte, 1500)
 	for {
-		<-time.After(time.Second)
-		counter++
-
-		err := binary.Write(s, binary.BigEndian, counter)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func readCounter(s network.Stream) {
-	for {
-		var counter uint64
-
-		err := binary.Read(s, binary.BigEndian, &counter)
+		n, err := conn.Read(buf)
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Received %d from %s\n", counter, s.ID())
+		fmt.Printf("Received: '%s'\n", string(buf[:n]))
 	}
-}
-
-type discoveryNotifee struct {
-	h host.Host
-}
-
-func (n *discoveryNotifee) HandlePeerFound(peerInfo peer.AddrInfo) {
-	fmt.Println("found peer", peerInfo.String())
 }
